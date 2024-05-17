7a9227f18dcdb5f0592706c7eee02bea251a9388
giadarol <giovanni.iadarola@cern.ch>
2024-05-17 20:18:23 +0200

    Manage to configure bb

--- examples/hllhc14_with_bb/000_single_da_sixtrack.py
+++ examples/hllhc14_with_bb/000_single_da_sixtrack.py
@@ -25,4 +25,23 @@ p0c_eV = sixinput.initialconditions[-3] * 1e6
 
 # Build line from sixtrack input
 line = sixinput.generate_xtrack_line()
+line.particle_ref = xt.Particles(p0c=p0c_eV)
+
+# Create knob to switch bb on/off
+tt = line.get_table()
+ttbb = tt.rows[(tt.element_type=='BeamBeamBiGaussian2D')
+               |(tt.element_type=='BeamBeamBiGaussian3D')]
+
+line.vars['beambeam_scale'] = 1.
+for nn in ttbb.name:
+    line.element_refs[nn].scale_strength = line.vars['beambeam_scale']
+
+
+# Twiss bb off
+line.vars['beambeam_scale'] = 0
+tw_nobb = line.twiss()
+line.vars['beambeam_scale'] = 1.
+
+xf.configure_orbit_dependent_parameters_for_bb(line,
+        particle_on_co=tw_nobb.particle_on_co)
 
