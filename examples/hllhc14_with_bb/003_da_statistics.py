812c5e4076ee979c287395ab9ca4de80e3f0c422
giadarol <giovanni.iadarola@cern.ch>
2024-05-24 22:59:57 +0200

    Clean up pictures

--- examples/hllhc14_with_bb/003_da_statistics.py
+++ examples/hllhc14_with_bb/003_da_statistics.py
@@ -8,6 +8,7 @@ import itertools
 
 import numpy as np
 import pandas as pd
+import statsmodels.api as sm
 
 import sixtracktools
 from sixtrack import track_particle_sixtrack
@@ -48,29 +49,109 @@ line.vars['beambeam_scale'] = 1.
 xf.configure_orbit_dependent_parameters_for_bb(line,
         particle_on_co=tw_nobb.particle_on_co)
 
-r_min_lost = []
+sets = {}
 
-for i_sim in range(16):
+for set_name in ['sixtrack', 'xsuite']:
 
-    with open(f'da_sim_{i_sim}.json', 'r') as fid:
-        data = json.load(fid)
+    tag = {'sixtrack': '', 'xsuite': '_xsuite'}[set_name]
 
-    particles = xp.Particles.from_dict(data['particles_init'])
-    last_turn = np.array(data['last_turn'])
+    r_min_lost = []
+    r_min_sxt_lost = []
 
-    norm = tw_nobb.get_normalized_coordinates(particles, nemitt_x=2.5e-6, nemitt_y=2.5e-6)
+    for i_sim in range(64):
 
-    r_init = np.sqrt(norm.x_norm**2 + norm.y_norm**2)
+        with open(f'da_sim_{i_sim}{tag}.json', 'r') as fid:
+            data = json.load(fid)
 
-    mask_lost = last_turn < 100000
+        particles = xp.Particles.from_dict(data['particles_init'])
+        last_turn = np.array(data['last_turn'])
 
-    min_r_lost_run = np.min(r_init[mask_lost])
+        norm = tw_nobb.get_normalized_coordinates(particles, nemitt_x=2.5e-6, nemitt_y=2.5e-6)
 
-    r_min_lost.append(min_r_lost_run)
+        r_init = np.sqrt(norm.x_norm**2 + norm.y_norm**2)
+
+        mask_lost = last_turn < 100000
+
+        min_r_lost_run = np.min(r_init[mask_lost])
+
+        r_min_lost.append(min_r_lost_run)
+
+        if 'f10' in data:
+            amplitude_x_sxt = np.zeros_like(r_init)
+            f10 = np.array(data['f10'])
+            amplitude_x_sxt[::2] = f10[:,6]
+            amplitude_x_sxt[1::2] = f10[:,7]
+
+            amplitude_y_sxt = np.zeros_like(r_init)
+            amplitude_y_sxt[::2] = f10[:,25]
+            amplitude_y_sxt[1::2] = f10[:,26]
+
+            r_init_sxt = np.sqrt(amplitude_x_sxt**2 + amplitude_y_sxt**2)
+            r_init_sxt[r_init_sxt == 0] = np.nan
+            min_r_sxt_lost_run = np.nanmin(r_init_sxt[mask_lost])
+            r_min_sxt_lost.append(min_r_sxt_lost_run)
+
+    sets[set_name] = {'r_min_lost': r_min_lost, 'r_min_sxt_lost': r_min_sxt_lost}
+
+    obstat = sm.nonparametric.KDEUnivariate(r_min_lost)
+    obstat.fit()
+    obstat.fit(bw=0.2)
+    density = obstat.evaluate
+    sets[set_name]['density'] = density
+
+x_density = np.linspace(3, 8, 10000)
+
+import matplotlib.pyplot as plt
+plt.close('all')
+
+mean_sixtrack = np.mean(sets['sixtrack']['r_min_lost'])
+mean_xsuite = np.mean(sets['xsuite']['r_min_lost'])
+std_sixtrack = np.std(sets['sixtrack']['r_min_lost'])
+std_xsuite = np.std(sets['xsuite']['r_min_lost'])
+
+density_sixtrack_gauss = np.exp(-(x_density - mean_sixtrack)**2/(2*std_sixtrack**2))/np.sqrt(2*np.pi*std_sixtrack**2)
+density_xsuite_gauss = np.exp(-(x_density - mean_xsuite)**2/(2*std_sixtrack**2))/np.sqrt(2*np.pi*std_sixtrack**2)
+density_sixtrack_gauss = density_sixtrack_gauss/np.max(density_sixtrack_gauss)
+density_xsuite_gauss = density_xsuite_gauss/np.max(density_xsuite_gauss)
+
+plt.figure(1, figsize=(6.4, 4.8 * 1.5))
+sp1 = plt.subplot(211)
+sp2 = plt.subplot(212, sharex=sp1)
+sp1.plot(sets['sixtrack']['r_min_lost'], np.zeros_like(sets['sixtrack']['r_min_lost']), '.', color='C0')
+sp2.plot(sets['xsuite']['r_min_lost'], -np.zeros_like(sets['xsuite']['r_min_lost']), '.', color='C1')
+
+sp1.axvline(np.mean(sets['sixtrack']['r_min_lost']), color='C0')
+sp2.axvline(np.mean(sets['xsuite']['r_min_lost']), color='C1')
+
+sp1.plot(x_density, density_sixtrack_gauss, color='C0')
+sp2.plot(x_density, density_xsuite_gauss, color='C1')
+sp1.set_title('Sixtrack (64 repetitions)')
+sp2.set_title('Xsuite (64 repetitions)')
+
+# Text box top right
+info_sixtrack = '\n'.join((
+    r'DA$_{mean}$ = ' + f'{mean_sixtrack:.2f}' + r' $\sigma$',
+    r'DA$_{std}$ = ' + f'{std_sixtrack:.2f}' + r' $\sigma$'
+    ))
+info_xsuite = '\n'.join((
+    r'DA$_{mean}$ = ' + f'{mean_xsuite:.2f}' + r' $\sigma$',
+    r'DA$_{std}$ = ' + f'{std_xsuite:.2f}' + r' $\sigma$'
+    ))
+props = dict(boxstyle='round', facecolor='white', alpha=0.5)
+sp1.text(0.03, 0.95, info_sixtrack, transform=sp1.transAxes, fontsize=12,
+        verticalalignment='top', bbox=props)
+sp2.text(0.03, 0.95, info_xsuite, transform=sp2.transAxes, fontsize=12,
+        verticalalignment='top', bbox=props)
+
+sp1.set_xlabel(r'DA [$\sigma$]')
+sp2.set_xlabel(r'DA [$\sigma$]')
+sp1.set_ylabel('Fitted density')
+sp2.set_ylabel('Fitted density')
+
+plt.subplots_adjust(hspace=0.31)
+plt.show()
 
 
-# import matplotlib.pyplot as plt
-# plt.close('all')
 # plt.figure(1)
 # plt.plot(r_init, last_turn, '.')
 
